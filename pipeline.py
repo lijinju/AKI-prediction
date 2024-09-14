3946f54fafa83f1bbd30a8e1e89069116babc60d
Foundry (vector-client-token) <b110951a-d0e6-43f6-9f69-8e66d71570d1>
2024-09-14 21:04:14 +0000

    Update workbook

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,64 @@
 
 
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.f6d9e4cb-b448-4693-b5bf-c971f42f084e")
+)
+def descisontree_infection(infection):
+  
+    from sklearn.model_selection import train_test_split
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.metrics import accuracy_score
+    from sklearn.metrics import roc_curve, auc
+    import matplotlib.pyplot as plt
+
+    # 准备数据
+    X = infection[['infection','gender', 'race', 'ethnicity', 'past_AKI', 'hypertension', 'diabetes_mellitus', 'heart_failure', 'cardiovascular_disease',  
+    'obesity']]
+    y = infection ['outcome']
+
+    # 划分训练集和测试集
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # 构建决策树模型
+    model = DecisionTreeClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    # 在测试集上进行预测
+    y_pred = model.predict(X_test)
+
+    # 计算准确率
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy:", accuracy)
+
+    # 计算AUC曲线
+    y_score = model.predict_proba(X_test)[:,1]
+    fpr, tpr, _ = roc_curve(y_test, y_score)
+    roc_auc = auc(fpr, tpr)
+
+    # 绘制AUC曲线
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic of DecisionTreeClassifier')
+    plt.legend(loc="lower right")
+    plt.show()
+
+    # 获取特征重要性
+    feature_importance = model.feature_importances_
+    print("Feature Importance:", feature_importance)
+
+    # 可视化特征重要性
+    plt.figure(figsize=(10, 6))
+    plt.barh(range(len(feature_importance)), feature_importance, align='center')
+    plt.yticks(range(len(feature_importance)), X.columns)
+    plt.xlabel('Feature Importance')
+    plt.ylabel('Features')
+    plt.title('Decision Tree Feature Importance')
+    plt.show()
+
+    
+
--- pipeline.sql
+++ pipeline.sql
@@ -1,2 +1,55 @@
 
 
+@transform_pandas(
+    Output(rid="ri.foundry.main.dataset.e6dc10f3-82c9-4ddd-8496-cc69f3756d25"),
+    Demographic_for_tableone=Input(rid="ri.foundry.main.dataset.77e107cc-6218-4565-86a9-754d545a72bd")
+)
+SELECT  has_AKI AS outcome, age_num AS age, CASE WHEN group_id = 1 then 1 else 2 end as vaccine_infection, ---previous: vaccine ,1;infection 0;now vaccine 1 infection 2
+CASE WHEN gender = 'M' THEN 1 ELSE 0 END AS gender,
+CASE WHEN race = 'white' THEN 0
+   WHEN race = 'black' THEN 1
+   WHEN race = 'no info' THEN 2
+   WHEN race = 'asian' THEN 3
+   WHEN race = 'other' THEN 4 ELSE 5 END AS race,
+CASE WHEN ethnicity_1 = 'Not Hispanic or Latino' THEN 0
+   WHEN ethnicity_1 = 'Hispanic or Latino' THEN 1
+   WHEN race = 'UNKNOWN' THEN 2 ELSE 3 END AS ethnicity,
+past_AKI, hypertension,diabetes_mellitus, heart_failure,cardiovascular_disease,obesity
+FROM Demographic_for_tableone ;
+
+@transform_pandas(
+    Output(rid="ri.vector.main.execute.0f2143fb-c73f-407d-b4e2-15bfe7dc4c88"),
+    Demographic_for_tableone=Input(rid="ri.foundry.main.dataset.77e107cc-6218-4565-86a9-754d545a72bd")
+)
+SELECT  person_id,has_AKI AS outcome,
+CASE WHEN gender = 'M' THEN 1 ELSE 0 END AS gender,
+CASE WHEN race = 'white' THEN 0
+   WHEN race = 'black' THEN 1
+   WHEN race = 'no info' THEN 2
+   WHEN race = 'asian' THEN 3
+   WHEN race = 'other' THEN 4 ELSE 5 END AS race,
+CASE WHEN ethnicity_1 = 'Not Hispanic or Latino' THEN 0
+   WHEN ethnicity_1 = 'Hispanic or Latino' THEN 1
+   WHEN race = 'UNKNOWN' THEN 2 ELSE 3 END AS ethnicity,
+past_AKI, hypertension,diabetes_mellitus, heart_failure,cardiovascular_disease,obesity
+FROM Demographic_for_tableone
+WHERE group_id = 1;
+
+@transform_pandas(
+    Output(rid="ri.foundry.main.dataset.83ec449a-947d-44d8-83a9-88e9743f0ab8"),
+    Demographic_for_tableone=Input(rid="ri.foundry.main.dataset.77e107cc-6218-4565-86a9-754d545a72bd")
+)
+SELECT  has_AKI AS outcome, age_num AS age, CASE WHEN group_id = 1 then 1 else 0 end as vaccine,
+CASE WHEN gender = 'M' THEN 1 ELSE 0 END AS gender,
+CASE WHEN race = 'white' THEN 0
+   WHEN race = 'black' THEN 1
+   WHEN race = 'no info' THEN 2
+   WHEN race = 'asian' THEN 3
+   WHEN race = 'other' THEN 4 ELSE 5 END AS race,
+CASE WHEN ethnicity_1 = 'Not Hispanic or Latino' THEN 0
+   WHEN ethnicity_1 = 'Hispanic or Latino' THEN 1
+   WHEN race = 'UNKNOWN' THEN 2 ELSE 3 END AS ethnicity,
+past_AKI, hypertension,diabetes_mellitus, heart_failure,cardiovascular_disease,obesity
+FROM Demographic_for_tableone 
+WHERE group_id=1;
+
